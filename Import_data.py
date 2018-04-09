133a7d55905ea410bc3a5757c2b5606e2964e829
ConorHogan <36172554+ConorHogan@users.noreply.github.com>
2018-04-09 21:47:44 +0100

    Print means for each column
    Updated to calculate means for each column and print

--- Import_data.py
+++ Import_data.py
@@ -3,10 +3,13 @@
 
 import pandas as pd
 import numpy as np
-
 url = 'https://raw.githubusercontent.com/ConorHogan/Programming_Scripting_Projects/master/Iris_Data.csv'
-
 df = pd.read_csv(url) # apparently you call this a df for dataframe
-df.columns = ["S-Length","S-Width","P-Length","P-Width","Class"] # Assigning column headers
+df.columns = ["S-Length","S-Width","P-Length","P-Width","Class"]
 
-print(df)
+meanSL = df["S-Length"].mean()
+meanSW = df["S-Width"].mean()
+meanPL = df["P-Length"].mean()
+meanPW = df["P-Width"].mean()
+
+print(meanSL,meanSW,meanPL,meanPW)
